60bab5b7acdb715362ef5ca329cf8e2595dc9498
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2025-08-15 20:54:58 +0530

    Time: 188 ms (63.80%) | Memory: 41.7 MB (14.11%) - LeetSync

---
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
+        diff = [[0] * (n + 1) for _ in range(n + 1)]
+
+        for r1, c1, r2, c2 in queries:
+            diff[r1][c1] += 1
+            diff[r1][c2 + 1] -= 1
+            diff[r2 + 1][c1] -= 1
+            diff[r2 + 1][c2 + 1] += 1
+
+        for r in range(n):
+            for c in range(1, n):
+                diff[r][c] += diff[r][c - 1]
+
+        for c in range(n):
+            for r in range(1, n):
+                diff[r][c] += diff[r - 1][c]
+
+        return [row[:n] for row in diff[:n]]
