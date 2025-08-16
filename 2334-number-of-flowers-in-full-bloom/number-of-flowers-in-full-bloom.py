eedbea7aa55afbfe9a522c2a029fd0d077524b39
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2025-08-17 00:55:54 +0530

    Time: 132 ms (45.42%) | Memory: 42.1 MB (42.32%) - LeetSync

--- 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.py
+++ 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.py
@@ -1,24 +1,45 @@
 class Solution:
     def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
-        people =[(p,i) for i,p in enumerate(people)]
+        people = [(p , i) for i , p in enumerate(people)]
         res = [0] * len(people)
+        flowers.sort()
+        end =[]
 
-        start = [f[0] for f in flowers]
-        end = [f[1] for f in flowers]
-        heapq.heapify(start)
-        heapq.heapify(end)
-        count = 0  
+        j = 0
 
         for p , i in sorted(people):
+            while j < len(flowers) and flowers[j][0] <= p:
+                heapq.heappush(end ,flowers[j][1])
+                j += 1
 
-            while start and start[0] <= p:
-                heapq.heappop(start)
-                count += 1
             while end and end[0] < p:
                 heapq.heappop(end)
-                count -= 1
-            res[i] = count
+
+            res[i] = len(end)
 
         return res
+        
+
+        
+        # people =[(p,i) for i,p in enumerate(people)]
+        # res = [0] * len(people)
+
+        # start = [f[0] for f in flowers]
+        # end = [f[1] for f in flowers]
+        # heapq.heapify(start)
+        # heapq.heapify(end)
+        # count = 0  
+
+        # for p , i in sorted(people):
+
+        #     while start and start[0] <= p:
+        #         heapq.heappop(start)
+        #         count += 1
+        #     while end and end[0] < p:
+        #         heapq.heappop(end)
+        #         count -= 1
+        #     res[i] = count
+
+        # return res
 
         
