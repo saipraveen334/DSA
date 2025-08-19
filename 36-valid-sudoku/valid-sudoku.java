a21a717801528bebd4f51b1da598acb80f5c10ea
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2025-08-19 20:59:47 +0530

    Time: 13 ms (21.27%) | Memory: 45.5 MB (5.25%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -2,30 +2,20 @@ import java.util.*;
 
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<Integer>[] rows = new HashSet[9];
-        HashSet<Integer>[] cols = new HashSet[9];
-        HashSet<Integer>[] boxes = new HashSet[9];
-
-        for (int i = 0; i < 9; i++) {
-            rows[i] = new HashSet<>();
-            cols[i] = new HashSet<>();
-            boxes[i] = new HashSet<>();
-        }
+        HashSet<String> seen = new HashSet<>();
 
         for (int r = 0; r < 9; r++) {
             for (int c = 0; c < 9; c++) {
-                if (board[r][c] == '.') continue;
+                char val = board[r][c];
+                if (val == '.') continue;
 
-                int val = board[r][c] - '0';
-                int boxIndex = (r / 3) * 3 + (c / 3);
+                String rowKey = val + " in row " + r;
+                String colKey = val + " in col " + c;
+                String boxKey = val + " in box " + (r / 3) + "-" + (c / 3);
 
-                if (rows[r].contains(val) || cols[c].contains(val) || boxes[boxIndex].contains(val)) {
+                if (!seen.add(rowKey) || !seen.add(colKey) || !seen.add(boxKey)) {
                     return false;
                 }
-
-                rows[r].add(val);
-                cols[c].add(val);
-                boxes[boxIndex].add(val);
             }
         }
         return true;
