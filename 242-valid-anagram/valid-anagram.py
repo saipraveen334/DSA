79694bf2f6e8d91d9c7d8cfcba36662592f30d23
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-03-02 15:28:45 +0530

    Time: 14 ms (46.40%) | Memory: 19.2 MB (89.53%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,5 +1,28 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+
+        # FREQUENCY ARRAY ----CRAZYYY 
+        if len(s) != len(t):
+            return False
+
+        count = [0] * 26
+
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+        
+        for val in count:
+            if val != 0:
+                return False
+        return True
+
+
+
+
+
+
+        # HASHMAP 
+
         if len(s) != len(t):
             return False
         
