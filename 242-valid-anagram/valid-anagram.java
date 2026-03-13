da2a66d8ca5ed4cd98aa22b62c187f37651fc6ed
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-03-13 14:47:10 +0530

    Time: 27 ms (5.16%) | Memory: 46.8 MB (7.89%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,30 +1,48 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        //USING FREQUENCY ARRAY
 
-        if(s.length() != t.length())
-        {
+        // USING HASHMAP
+        if (s.length() != t.length()) {
             return false;
         }
-        
 
-        int count[] = new int[26];
+        HashMap<Character, Integer> hm1 = new HashMap<>();
+        HashMap<Character, Integer> hm2 = new HashMap<>();
 
-        for(int i = 0 ; i < s.length() ; i++)
-        {
-            count[s.charAt(i) - 'a'] += 1;
-            count[t.charAt(i) - 'a'] -= 1;
+        for (int i = 0; i < s.length(); i++) {
+            hm1.put(s.charAt(i), hm1.getOrDefault(s.charAt(i), 0) + 1);
+            hm2.put(t.charAt(i), hm2.getOrDefault(t.charAt(i), 0) + 1);
         }
 
-        for (int val : count)
-        {
-            if(val != 0)
-            {
-                return false;
-            }
-        }
-        return true;
-
-        
+        return hm1.equals(hm2);
     }
-}
+    }
+
+        //USING FREQUENCY ARRAY
+
+//         if(s.length() != t.length())
+//         {
+//             return false;
+//         }
+        
+
+//         int count[] = new int[26];
+
+//         for(int i = 0 ; i < s.length() ; i++)
+//         {
+//             count[s.charAt(i) - 'a'] += 1;
+//             count[t.charAt(i) - 'a'] -= 1;
+//         }
+
+//         for (int val : count)
+//         {
+//             if(val != 0)
+//             {
+//                 return false;
+//             }
+//         }
+//         return true;
+
+        
+//     }
+// }
