5e0038abeb46542eee4b259632ebf13ddc3119eb
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-03-13 14:48:45 +0530

    Time: 5 ms (60.27%) | Memory: 44.1 MB (94.37%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,24 +1,52 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        //USING FREQUENCY ARRAY
 
-        // USING HASHMAP
-        if (s.length() != t.length()) {
+        if(s.length() != t.length())
+        {
             return false;
         }
+        
 
-        HashMap<Character, Integer> hm1 = new HashMap<>();
-        HashMap<Character, Integer> hm2 = new HashMap<>();
+        int count[] = new int[26];
 
-        for (int i = 0; i < s.length(); i++) {
-            hm1.put(s.charAt(i), hm1.getOrDefault(s.charAt(i), 0) + 1);
-            hm2.put(t.charAt(i), hm2.getOrDefault(t.charAt(i), 0) + 1);
+        for(int i = 0 ; i < s.length() ; i++)
+        {
+            count[s.charAt(i) - 'a'] += 1;
+            count[t.charAt(i) - 'a'] -= 1;
         }
 
-        return hm1.equals(hm2);
-    }
-    }
+        for (int val : count)
+        {
+            if(val != 0)
+            {
+                return false;
+            }
+        }
+        return true;
 
-        //USING FREQUENCY ARRAY
+        
+    }
+}
+
+        // USING HASHMAP
+//         if (s.length() != t.length())
+//         {
+//             return false;
+//         }
+
+//         HashMap<Character, Integer> hm1 = new HashMap<>();
+//         HashMap<Character, Integer> hm2 = new HashMap<>();
+
+//         for(int i = 0 ; i < s.length() ; i++)
+//         {
+//             hm1.put(s.charAt(i) , hm1.getOrDefault(s.charAt(i) , 0) + 1);
+//             hm2.put(t.charAt(i) , hm2.getOrDefault(s.charAt(i) , 0) + 1);
+//         }
+//         return hm1.equals(hm2);
+//     }
+
+//         //USING FREQUENCY ARRAY
 
 //         if(s.length() != t.length())
 //         {
