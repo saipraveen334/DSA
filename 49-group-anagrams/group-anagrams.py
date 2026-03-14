0e9b0f42191169182ea3b5687fbc8de1afd10252
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-03-14 15:47:23 +0530

    Time: 17 ms (27.19%) | Memory: 24.1 MB (9.76%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -14,21 +14,21 @@ class Solution:
                 res[key] = []
             
             res[key].append(s)
-            
-        return list(res.values())
-
-
-        res = defaultdict(list)
-
-        for s in strs:
-
-            count = [0] * 26
-            
-            for c in s:
-                count[ord(c) - ord('a')] += 1
-            
-            res[tuple(count)].append(s)
 
         return list(res.values())
+
+
+        # res = defaultdict(list)
+
+        # for s in strs:
+
+        #     count = [0] * 26
+            
+        #     for c in s:
+        #         count[ord(c) - ord('a')] += 1
+            
+        #     res[tuple(count)].append(s)
+
+        # return list(res.values())
 
         
