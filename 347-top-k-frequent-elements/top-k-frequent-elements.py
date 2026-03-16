ed4f4d64ac5e05bf649d4286a5e94b34f8a67e20
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-03-16 16:46:16 +0530

    Time: 7 ms (51.15%) | Memory: 23 MB (56.30%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,22 +1,47 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freqcount = {} # NUMBER - > FREQUENCY COUNT
+
         res = []
-        FrequencyMap = {} # number -> frequency count
 
         for n in nums:
-            FrequencyMap[n] = 1 + FrequencyMap.get(n,0)
-        
-        bucket =[[] for _ in range(len(nums)+1)]
+            freqcount[n] = 1 + freqcount.get(n,0)
 
-        for n,fcount in FrequencyMap.items():
-            bucket[fcount].append(n) 
-        
-        for i in range(len(nums) , 0 , -1):
-            for num in bucket[i]:
-                res.append(num)
+        maxheap = []
 
-                if k == len(res):
-                    return res
+        for num , count in freqcount.items():
+            heapq.heappush(maxheap, (-count , num))
+        
+        while maxheap and k > 0:
+            val , n = heapq.heappop(maxheap)
+            res.append(n)
+            k-= 1
+
+        return res
+
+        
+
+        
+
+
+
+        # res = []
+        # FrequencyMap = {} # number -> frequency count
+
+        # for n in nums:
+        #     FrequencyMap[n] = 1 + FrequencyMap.get(n,0)
+        
+        # bucket =[[] for _ in range(len(nums)+1)]
+
+        # for n,fcount in FrequencyMap.items():
+        #     bucket[fcount].append(n) 
+        
+        # for i in range(len(nums) , 0 , -1):
+        #     for num in bucket[i]:
+        #         res.append(num)
+
+        #         if k == len(res):
+        #             return res
             
 
 
