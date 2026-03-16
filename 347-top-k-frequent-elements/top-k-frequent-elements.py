7568a18451f838cecb35568e5bb008ff86e70938
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-03-16 16:48:12 +0530

    Time: 7 ms (51.15%) | Memory: 23.1 MB (40.50%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,5 +1,8 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # USING MAXHEAP (AS MINHEAP WITH NEGATIVE VALUES AND HASHMAP APPROACH )
+
+
         freqcount = {} # NUMBER - > FREQUENCY COUNT
 
         res = []
@@ -21,27 +24,25 @@ class Solution:
 
         
 
+
+        # USING HASHMAP WITH BUCKET SORT INSANE SOL
+        res = []
+        FrequencyMap = {} # number -> frequency count
+
+        for n in nums:
+            FrequencyMap[n] = 1 + FrequencyMap.get(n,0)
         
+        bucket =[[] for _ in range(len(nums)+1)]
 
-
-
-        # res = []
-        # FrequencyMap = {} # number -> frequency count
-
-        # for n in nums:
-        #     FrequencyMap[n] = 1 + FrequencyMap.get(n,0)
+        for n,fcount in FrequencyMap.items():
+            bucket[fcount].append(n) 
         
-        # bucket =[[] for _ in range(len(nums)+1)]
+        for i in range(len(nums) , 0 , -1):
+            for num in bucket[i]:
+                res.append(num)
 
-        # for n,fcount in FrequencyMap.items():
-        #     bucket[fcount].append(n) 
-        
-        # for i in range(len(nums) , 0 , -1):
-        #     for num in bucket[i]:
-        #         res.append(num)
-
-        #         if k == len(res):
-        #             return res
+                if k == len(res):
+                    return res
             
 
 
