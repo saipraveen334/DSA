3e2780e27c0bb09f2fb457275b57ad19c5931746
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-03-17 18:12:15 +0530

    Time: 11 ms (95.67%) | Memory: 51.9 MB (5.38%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,27 +1,62 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        // USING HASHMAP AND BUCKET SORT 
+
+        HashMap<Integer , Integer> FreqCount = new HashMap<>();
+
+        for( int n : nums)
+        {
+            FreqCount.put(n , FreqCount.getOrDefault(n , 0) + 1);
+        }
+
+        List<Integer>[] bucket = new List[nums.length + 1];
+
+        for(int i = 0; i < bucket.length; i++){
+            bucket[i] = new ArrayList<>();
+        }
+
+        int res[] = new int[k];
+
+        for(Map.Entry<Integer ,Integer> entry: FreqCount.entrySet())
+        {
+            bucket[entry.getValue()].add(entry.getKey());
+        }
+        int index = 0;
+
+        for(int i = bucket.length - 1; i >= 0 && index < k; i--){
+            for(int num : bucket[i]){
+                res[index++] = num;
+                if(index == k) break;
+            }
+        }
+        return res;
+    }
+}
+
+
+
         // USING HASHMAP AND PRIORITY QUEUE 
 
-        HashMap<Integer , Integer> FrequencyCount = new HashMap<>();
+//         HashMap<Integer , Integer> FrequencyCount = new HashMap<>();
 
-        for( int i = 0 ; i < nums.length ; i++)
-        {
-            FrequencyCount.put(nums[i] , FrequencyCount.getOrDefault(nums[i],0) + 1);
-        }
+//         for( int i = 0 ; i < nums.length ; i++)
+//         {
+//             FrequencyCount.put(nums[i] , FrequencyCount.getOrDefault(nums[i],0) + 1);
+//         }
 
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> b[1] - a[1]);
-        for(Map.Entry<Integer,Integer> entry : FrequencyCount.entrySet()){
-            maxHeap.add(new int[]{entry.getKey(), entry.getValue()});
-        }
-        int[] res = new int[k];
+//         PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> b[1] - a[1]);
+//         for(Map.Entry<Integer,Integer> entry : FrequencyCount.entrySet()){
+//             maxHeap.add(new int[]{entry.getKey(), entry.getValue()});
+//         }
+//         int[] res = new int[k];
 
-        for(int i = 0; i < k; i++){
-            res[i] = maxHeap.poll()[0];
-        }
+//         for(int i = 0; i < k; i++){
+//             res[i] = maxHeap.poll()[0];
+//         }
 
-        return res;
+//         return res;
 
 
         
-    }
-}
+//     }
+// }
