067035138c0448574c7f5f7cd921d004aced9603
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-03-30 02:36:50 +0530

    Time: 7 ms (28.95%) | Memory: 20.5 MB (86.48%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -17,14 +17,14 @@ class Solution:
 
 
         # USING HASHMAP 
-        # seen = defaultdict(int) 
+        seen = defaultdict(int) 
 
-        # for i in range(len(numbers)):
-        #     diff = target - numbers[i]
+        for i in range(len(numbers)):
+            diff = target - numbers[i]
 
-        #     if diff in seen:
-        #         return [seen[diff] + 1, i + 1]
+            if diff in seen:
+                return [seen[diff] + 1, i + 1]
             
-        #     seen[numbers[i]] = i
-        # return []
+            seen[numbers[i]] = i
+        return []
         
