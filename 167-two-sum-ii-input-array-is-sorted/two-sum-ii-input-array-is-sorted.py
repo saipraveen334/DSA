3c276f0ce0a0d6c5cc6f990a01395fc0a25157df
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-03-30 02:32:24 +0530

    Time: 3 ms (80.05%) | Memory: 20.4 MB (97.48%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,6 +1,6 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        seen = {} # value -- > index 
+        seen = defaultdict(int) 
 
         for i in range(len(numbers)):
             diff = target - numbers[i]
@@ -9,4 +9,5 @@ class Solution:
                 return [seen[diff] + 1, i + 1]
             
             seen[numbers[i]] = i
+        return []
         
