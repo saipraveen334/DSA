a5bc54d934b804e84d68740f731348c197291daa
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-04-03 09:26:54 +0530

    Time: 385 ms (9.33%) | Memory: 47.6 MB (5.02%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -20,26 +20,26 @@ class Solution:
 
 
 
-        # res = []
+        res = []
 
-        # q = deque() #INDEX 
-        # l = r = 0
+        q = deque() #INDEX 
+        l = r = 0
 
-        # while r < len(nums):
+        while r < len(nums):
 
-        #     while q and nums[r] > nums[q[-1]]:
-        #         q.pop()
+            while q and nums[r] > nums[q[-1]]:
+                q.pop()
 
-        #     q.append(r)
+            q.append(r)
 
-        #     if l > q[0]:
-        #         q.popleft()
+            if l > q[0]:
+                q.popleft()
             
-        #     if (r + 1) >= k:
-        #         res.append(nums[q[0]])
-        #         l += 1
-        #     r += 1
-        # return res
+            if (r + 1) >= k:
+                res.append(nums[q[0]])
+                l += 1
+            r += 1
+        return res
         
 
             
