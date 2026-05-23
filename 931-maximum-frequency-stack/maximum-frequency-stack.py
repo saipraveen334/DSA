4e73a54ed99fb75fce837baf5157693bfd72abe0
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-05-23 15:34:27 +0530

    Time: 67 ms (78.92%) | Memory: 27.9 MB (64.86%) - LeetSync

--- 931-maximum-frequency-stack/maximum-frequency-stack.py
+++ 931-maximum-frequency-stack/maximum-frequency-stack.py
@@ -1,23 +1,28 @@
 class FreqStack:
-
     #INSANE SOLUTION 
-    
+
     def __init__(self):
         self.cnt = {}
-        self.maxCnt = 0
         self.stack = {}
+        self.maxCnt = 0
+        
 
     def push(self, val: int) -> None:
+
         valCnt = 1 + self.cnt.get(val , 0)
+
         self.cnt[val] = valCnt
 
         if valCnt > self.maxCnt:
             self.maxCnt = valCnt
+
             self.stack[self.maxCnt] = []
 
-        self.stack[valCnt].append(val)        
+        self.stack[valCnt].append(val)
+        
 
     def pop(self) -> int:
+
         res = self.stack[self.maxCnt].pop()
 
         self.cnt[res] -= 1
