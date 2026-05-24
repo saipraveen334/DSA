f30354fbdd10beb3a09aabfa963d1aa20d56572e
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-05-24 18:39:47 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (47.06%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -3,14 +3,20 @@ class Solution {
         int l = 0;
         int r = nums.length - 1;
 
-        while (l <= r) {
+        while (l <= r)
+        {
             int mid = l + (r - l) / 2;
 
-            if (nums[mid] == target) {
+            if (nums[mid] == target)
+            {
                 return mid;
-            } else if (nums[mid] > target) {
+            }
+            else if (nums[mid] > target)
+            {
                 r = mid - 1;
-            } else {
+            }
+            else
+            {
                 l = mid + 1;
             }
         }
