c6d6e7852e31a2c2b0c826f3c3fb4f6926c9d237
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-06-11 00:32:12 +0530

    Time: 2 ms (88.93%) | Memory: 49.2 MB (70.32%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -1,21 +1,34 @@
-class Solution
-{
-    public void reorderList(ListNode head)
-    {
-        ListNode slow = head;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+
+        ListNode  slow = head;
         ListNode fast = head;
 
-        while (fast != null && fast.next != null)
+        while(fast!= null && fast.next != null)
         {
-            slow = slow.next;
+            slow = slow.next; 
             fast = fast.next.next;
         }
 
+        //REVERSE THE SECOND CHAIN 
+
         ListNode second = slow.next;
+
         ListNode prev = null;
+
         slow.next = null;
 
-        while (second != null)
+        while( second != null)
         {
             ListNode nxt = second.next;
             second.next = prev;
@@ -23,17 +36,23 @@ class Solution
             second = nxt;
         }
 
+        // USE TWO POINTERS TO REORDER LIST 
         ListNode first = head;
-        second = prev;
+        ListNode sec = prev ;
 
-        while (second != null)
+        while(sec != null)
         {
-            ListNode tmp1 = first.next;
-            ListNode tmp2 = second.next;
-            first.next = second;
-            second.next = tmp1;
-            first = tmp1;
-            second = tmp2;
+            ListNode temp1 = first.next;
+            ListNode temp2 = sec.next;
+
+            first.next = sec;
+            sec.next = temp1;
+
+            first = temp1;
+            sec = temp2;
         }
+        
+        
+        
     }
 }
