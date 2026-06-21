3dd09a32dd4dc2eece62dc6d56dde157cdfe82dd
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-06-21 18:53:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (50.75%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -17,20 +17,43 @@ class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
 
         // RECURSIVE APPROACH
-        List<Integer> res = new ArrayList<Integer>();
-        inorder(root , res );
+        // List<Integer> res = new ArrayList<Integer>();
+        // inorder(root , res );
+
+        //ITERATIVE APPROACH 
+
+        TreeNode cur = root;
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        List<Integer> res = new ArrayList<>();
+
+        while(cur != null || !stack.isEmpty())
+        {
+            while(cur != null)
+            {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            res.add(cur.val);
+            cur = cur.right;
+        }
         return res;
 
+
+
+        
+
     }
-    private void inorder(TreeNode root , List<Integer> res)
-        {
-            if(root == null)
-            {
-                return;
-            }
-            inorder(root.left , res);
-            res.add(root.val);
-            inorder(root.right , res);
-        }
+    // private void inorder(TreeNode root , List<Integer> res)
+    //     {
+    //         if(root == null)
+    //         {
+    //             return;
+    //         }
+    //         inorder(root.left , res);
+    //         res.add(root.val);
+    //         inorder(root.right , res);
+    //     }
+    // return res;
         
 }
