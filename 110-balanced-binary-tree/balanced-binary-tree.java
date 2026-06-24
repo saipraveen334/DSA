09ca084ce6c246d0a7eda792eb889561e150d976
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-06-25 00:38:38 +0530

    Time: 1 ms (31.17%) | Memory: 46.3 MB (5.23%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -0,0 +1,22 @@
+class Solution
+{
+    public boolean isBalanced(TreeNode root)
+    {
+        return dfs(root)[0] == 1;
+    }
+
+    private int[] dfs(TreeNode root)
+    {
+        if (root == null)
+        {
+            return new int[]{1, 0};
+        }
+
+        int[] left = dfs(root.left);
+        int[] right = dfs(root.right);
+
+        int balanced = (left[0] == 1 && right[0] == 1 && Math.abs(left[1] - right[1]) <= 1) ? 1 : 0;
+
+        return new int[]{balanced, 1 + Math.max(left[1], right[1])};
+    }
+}
