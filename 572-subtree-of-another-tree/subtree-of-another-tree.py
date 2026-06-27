eca46292bdd9f461bc1eceab65817c54b1f7313e
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-06-27 17:07:05 +0530

    Time: 23 ms (91.93%) | Memory: 19.7 MB (45.27%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -6,6 +6,25 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        # DFS + DFS APPROACH 
+        if not subRoot:
+            return True 
+        if not root:
+            return False 
+        
+        if self.sameTree(root , subRoot):
+            return True 
+        
+        return self.isSubtree(root.left , subRoot) or self.isSubtree(root.right , subRoot)
+    
+    def sameTree( self , p : Optional[TreeNode] , q: Optional[TreeNode]):
+        if not p and not q:
+            return True 
+        if p and q and p.val == q.val:
+            return self.sameTree(p.right , q.right) and self.sameTree(p.left , q.left)
+        else:
+            return False 
+
         q = deque([root])
 
         while q:
