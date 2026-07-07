6e467faa2466169b18df91f6c49f98a69a7afcfb
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-07-07 21:11:26 +0530

    Time: 3 ms (40.41%) | Memory: 19.9 MB (13.21%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
@@ -6,7 +6,45 @@
 #         self.right = right
 class Solution:
     def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
-        #RECURSIVE APPROACH 
+        # POST ORDER TRAVERSAL REMOVAL METHOD 
+        stack = [root]
+        visit = set()
+        parents = { root : None }
+
+        while stack:
+            node = stack.pop()
+
+            if not node.right and not node.left and target == node.val:
+                p = parents[node]
+
+                if not p:
+                    return None
+
+                # PART EXACTLY WHERE THE NODE GETS UNLINKED (DELETED) 
+
+                if p and p.left == node:
+                    p.left = None 
+                if p and p.right == node:
+                    p.right = None
+
+            elif node not in visit:
+                visit.add(node)
+                stack.append(node)
+
+                if node.left:
+                    stack.append(node.left)
+                    parents[node.left] = node 
+                if node.right:
+                    stack.append(node.right)
+                    parents[node.right] = node
+        return root
+
+                
+                
+
+
+
+        # RECURSIVE APPROACH 
 
         if not root:
             return None 
