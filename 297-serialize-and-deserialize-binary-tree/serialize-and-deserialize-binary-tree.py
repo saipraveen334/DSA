d153c9a07848e5a177161ec4c48c8c042d6fe098
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-07-14 10:43:52 +0530

    Time: 95 ms (28.92%) | Memory: 22.3 MB (86.63%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -6,49 +6,100 @@
 #         self.right = None
 
 class Codec:
-
     def serialize(self, root):
-        
-        #USING DFS 
+        # USING BFS 
+
+        # BASE CASE 
+        if not root:
+            return "N"
 
         res = []
+        q = deque([root])
 
-        def dfs(node):
-            if not node:
-                res.append("N")
-                return 
-            
-            res.append(str(node.val))
-            dfs(node.left)
-            dfs(node.right)
-            
-        dfs(root)
+        while q:
+            for _ in range(len(q)):
+                node = q.popleft()
+
+                if node:
+                    res.append(str(node.val))
+                    q.append(node.left)
+                    q.append(node.right)
+                else:
+                    res.append("N")
+        
         return ",".join(res)
+
+    def deserialize(self, data):
+        vals = data.split(",")
+
+        if vals[0] == "N":
+            return None 
+        
+        root = TreeNode(int(vals[0]))
+
+        q = deque([root])
+
+        index = 1
+
+        while q:
+            node = q.popleft()
+
+            if vals[index] != "N":
+                node.left = TreeNode(int(vals[index]))
+                q.append(node.left)
+            
+            index += 1
+
+            if vals[index] != "N":
+                node.right = TreeNode(int(vals[index]))
+                q.append(node.right)
+            
+            index += 1
+
+        return root
+
+    # def serialize(self, root):
+        
+    #     #USING DFS 
+
+    #     res = []
+
+    #     def dfs(node):
+    #         if not node:
+    #             res.append("N")
+    #             return 
+            
+    #         res.append(str(node.val))
+    #         dfs(node.left)
+    #         dfs(node.right)
+            
+    #     dfs(root)
+    #     return ",".join(res)
 
        
         
         
 
-    def deserialize(self, data):
-        vals = data.split(",")
+    # def deserialize(self, data):
+    #     vals = data.split(",")
 
-        i = 0 
+    #     i = 0 
 
-        def dfs():
-            nonlocal i
-            if vals[i] == "N":
-                i += 1
-                return None 
+    #     def dfs():
+    #         nonlocal i
+    #         if vals[i] == "N":
+    #             i += 1
+    #             return None 
             
-            node = TreeNode(int(vals[i]))
+    #         node = TreeNode(int(vals[i]))
 
-            i += 1
+    #         i += 1
             
-            node.left = dfs()
-            node.right = dfs()
-            return node
+    #         node.left = dfs()
+    #         node.right = dfs()
+    #         return node
 
-        return dfs()
+    #     return dfs()
        
 
 # Your Codec object will be instantiated and called as such:
