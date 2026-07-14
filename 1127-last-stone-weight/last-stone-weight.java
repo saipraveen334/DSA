4cacf1f3e33f2929340b0e3529da20cd98d6d02b
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-07-15 00:14:31 +0530

    Time: 1 ms (99.50%) | Memory: 42.7 MB (70.36%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -0,0 +1,25 @@
+import java.util.PriorityQueue;
+import java.util.Collections;
+
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int stone : stones) {
+            maxHeap.offer(stone);
+        }
+
+        while (maxHeap.size() > 1) {
+
+            int stone1 = maxHeap.poll();
+            int stone2 = maxHeap.poll();
+
+            if (stone1 != stone2) {
+                maxHeap.offer(stone1 - stone2);
+            }
+        }
+
+        return maxHeap.isEmpty() ? 0 : maxHeap.peek();
+    }
+}
