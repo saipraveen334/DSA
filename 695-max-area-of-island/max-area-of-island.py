28ac98339f35dbb2506ef9abb486bc9a5a25bfd8
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-08-21 00:53:04 +0530

    Time: 19 ms (61.69%) | Memory: 19.4 MB (95.68%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,5 +1,49 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+
+        # BFS APPROACH 
+        directions = [[0 , -1 ] , [1 , 0] , [ -1 , 0] , [0 ,1]]
+        area = 0
+
+        def bfs(r, c):
+            q = deque()
+            grid[r][c] = 0
+            q.append((r , c))
+            res = 1
+
+            while q:
+                row, col = q.popleft()
+
+                for dr , dc in directions:
+                    nr = dr + row
+                    nc = dc + col
+
+                    if nr < 0 or nc < 0 or nr >= len(grid) or nc >= len(grid[0]) or grid[nr][nc] == 0:
+                        continue 
+                    q.append((nr , nc))
+                    grid[nr][nc] = 0
+                    res += 1
+            return res
+                
+
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
+                    area = max(area , bfs(r , c))
+        return area
+
+
+
+
+
+
+
+
+
+
+
+
+
         # DFS APPROACH 
 
         visit = set()
