024186b2708f05ed89517f4b4b2b7d22dc91526a
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-09-02 18:50:40 +0530

    Time: 89 ms (37.47%) | Memory: 63 MB (48.72%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -1,44 +1,53 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
 
         # get length
-        length = 0
-        curr = head
+        length = 0 
+        curr = head 
 
         while curr:
             length += 1
-            curr = curr.next
-
+            curr = curr.next 
+        
         if length < 3:
-            return [-1, -1]
-
+            return [-1 , -1]
+        
         prev = head
-        cur = head.next
+        cur = head.next 
         criticalPoint = []
 
         for i in range(length - 2):
-
-            if (prev.val > cur.val < cur.next.val) or \
-               (prev.val < cur.val > cur.next.val):
+            if prev.val > cur.val < cur.next.val:
                 criticalPoint.append(i + 1)
-
+            if prev.val < cur.val > cur.next.val:
+                criticalPoint.append(i + 1)
+            
             cur = cur.next
-            prev = prev.next
+            prev = prev.next 
 
-        # fewer than 2 critical points
+        # what if there is only 1 critical point 
         if len(criticalPoint) < 2:
-            return [-1, -1]
-
-        # minimum distance
-        mindis = length
-
+            return [-1 , -1]
+        
+        # for calculatind mindis 
+        mindis = length 
         for i in range(len(criticalPoint) - 1):
-            mindis = min(
-                mindis,
-                criticalPoint[i + 1] - criticalPoint[i]
-            )
-
-        # maximum distance
+            mindis = min((criticalPoint[i + 1] - criticalPoint[i]), mindis)
+        
         maxdis = criticalPoint[-1] - criticalPoint[0]
 
-        return [mindis, maxdis]
+        return [mindis , maxdis]
+            
+        
+
+        
+            
+
+
+
+        
