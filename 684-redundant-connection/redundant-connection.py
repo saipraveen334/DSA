282b76af3fb13c66aafdc75f79cff01497b20530
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-09-06 03:43:35 +0530

    Time: 3 ms (60.93%) | Memory: 19.8 MB (27.84%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,6 +1,57 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        # peak dfs appraoch also
+        # topological sort 
+        adj = [[] for _ in range(len(edges) + 1)]
+
+        indegree = [0] * (len(edges) + 1) 
+        for u , v in edges:
+            adj[v].append(u)
+            adj[u].append(v)
+
+            indegree[u] += 1
+            indegree[v] += 1
+        
+        # add the node to queue that has one degree because its a leaf , not a part of cycle 
+
+        q = collections.deque()
+        
+        for i in range( 1 , len(edges) + 1):
+            if indegree[i] == 1:
+                q.append(i)
+
+        while q:
+            node = q.popleft()
+
+            indegree[node] -= 1
+
+            for nei in adj[node]:
+                indegree[nei] -= 1
+
+                if indegree[nei] == 1:
+                    q.append(nei)
+
+        # reversed becuase probably the last edge added to cycle to be removed 
+        for u , v in reversed(edges):
+            if indegree[u] == 2 and indegree[v] != 0:
+                return[u , v]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # dfs appraoch also
         # checking if we can reach the edge before adding it 
 
         adj = [[] for _ in range(len(edges) + 1)]
@@ -37,13 +88,6 @@ class Solution:
             adj[n2].append(n1)
         
         return [] # no cycle 
-
-            
-
-
-
-
-
 
 
 
