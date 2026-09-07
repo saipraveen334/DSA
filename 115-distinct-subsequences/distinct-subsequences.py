cf2d3593986d100a378aa7d3bfa05afff08cbd13
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-09-07 21:45:17 +0530

    Time: 870 ms (10.86%) | Memory: 254.4 MB (6.26%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,30 +1,57 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        if len(t)>len(s):
-            return 0
+        # cacheing + dfs to avoid time limit exceeded
 
-        dp={}
+        dp = {}
+        res = 0
 
-        def dfs(i,j):
-
-            if j==len(t):
+        def dfs(i , j):
+            if j == len(t):
                 return 1
 
-            if i==len(s):
-                return 0
-
-            if (i,j) in dp:
-                return dp[(i,j)]
-
-            res=dfs(i+1,j)
+            if i == len(s):
+                return 0 
             
-            if s[i]==t[j]: 
-                res+= dfs(i+1,j+1)
+            if (i , j) in dp:
+                return dp[(i, j)]
             
+            # not macthing the current charcter or trying the possible combinations 
 
-            dp[(i,j)]=res
+            res = dfs( i + 1 , j)
+
+            if s[i] == t[j]:
+                res += dfs( i + 1 , j + 1)
+            
+            dp[(i , j)] = res
+
             return res
 
-        return dfs(0,0)
+        return dfs(0, 0)
 
+
+
+        # time limit exceeded dfs 
+
+        res = 0
+
+        if len(t) > len(s):
+            return 0 
+
+
+        def dfs(i , j):
+            if j == len(t):
+                return 1
             
+            if i == len(s):
+                return 0
+            
+            # skip the charcter to find is there more possibilites or dont match the current character 
+            res = dfs(i + 1 , j)
+
+            if s[i] == t[j]:
+                res += dfs(i + 1 , j + 1)
+            
+            return res
+
+        return dfs(0 , 0)
+        
