ef7536c69e949d59447dd63ee059575554f78f5c
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-09-08 17:42:05 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (76.23%) - LeetSync

---
+++ 399-evaluate-division/evaluate-division.py
@@ -0,0 +1,43 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        adj = collections.defaultdict(list)
+
+
+        for i , eq in enumerate(equations):
+            e1 , e2 = eq 
+
+            adj[e1].append([e2 , values[i]]) 
+            adj[e2].append([e1 , 1 / values[i]]) # in reverse dir
+        
+        def bfs(src , target):
+            if not src in adj  or not target in adj:
+                return -1
+            
+            q = collections.deque()
+            visit = set()
+
+            q.append([src , 1])
+            visit.add(src)
+
+            while q:
+                node , w = q.popleft()
+
+                # base case 
+
+                if node == target:
+                    return w
+
+                for nei , weight  in adj[node]:
+                    if nei not in visit:
+                        q.append([nei , w * weight])
+                        visit.add(nei)
+            return -1 
+                
+
+        return [bfs(e[0] , e[1]) for e in queries]
+
+
+
+
+
+        
