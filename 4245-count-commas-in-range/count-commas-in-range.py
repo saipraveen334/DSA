9630f51f25bee9d3ce5e95a247c192da3d9805e5
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-09-08 16:41:31 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (46.53%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.py
+++ 4245-count-commas-in-range/count-commas-in-range.py
@@ -1,5 +1,17 @@
 class Solution:
     def countCommas(self, n: int) -> int:
+        # optimal sol
+
+        res = 0
+        power = 1000
+
+        while power <= n:
+            res += n - power + 1
+            power *= 1000
+
+        return res
+
+        
         # optimal solution 
 
         if n < 1000:
