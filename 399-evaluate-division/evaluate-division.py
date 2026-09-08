ba4c486bf0749379efd858a7ba5a4cd5abafcd98
SAI PRAVEEN <127715380+saipraveen334@users.noreply.github.com>
2026-09-09 00:10:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (12.47%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -1,5 +1,35 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        # DFS VERSION 
+        adj = collections.defaultdict(list)
+
+        for i , eq in enumerate(equations):
+            e1 , e2 = eq
+            adj[e1].append([e2 , values[i]])
+            adj[e2].append([e1 , 1 / values[i]])
+
+        def dfs(src, target, visited):
+            if src not in adj or target not in adj:
+                return -1
+            if src == target:
+                return 1
+
+            visited.add(src)
+
+            for nei, weight in adj[src]:
+                if nei not in visited:
+                    result = dfs(nei, target, visited)
+                    if result != -1:
+                        return weight * result
+
+            return -1
+
+        return [dfs(q[0], q[1], set()) for q in queries]
+
+
+
+        # BFS VERSION 
+
         adj = collections.defaultdict(list)
 
 
